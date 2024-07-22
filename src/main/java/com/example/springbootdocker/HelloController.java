e0fd205f7fa2e1ceab5ccaf601d657976e755255
danielebufaloclebari <daniele.bufalo@clebari.com>
2024-07-22 16:43:20 +0200

    test of version1

--- src/main/java/com/example/springbootdocker/HelloController.java
+++ src/main/java/com/example/springbootdocker/HelloController.java
@@ -8,11 +8,15 @@ public class HelloController {
 
     @RequestMapping("/")
     String hello() {
-        return "Hello 2 World, Spring Boot!";
+		System.out.println("20240722 Ciccio out Hello2");
+		System.err.println("20240722 Ciccio err Hello2");
+        return "20240722 Hello 2 World, Spring Boot!";
     }
     
     @RequestMapping("/hello2/{name}")
     String hello2(@PathVariable String name) {
+		System.out.println("20240722 Ciccio out Hello2 "+name);
+		System.err.println("20240722 Ciccio err Hello2 "+name);
         return "Hello 2 World !"+name.toString();
     }
 
--- src/main/java/com/example/springbootdocker/SpringBootDockerApplication.java
+++ src/main/java/com/example/springbootdocker/SpringBootDockerApplication.java
@@ -9,12 +9,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class SpringBootDockerApplication {
 
 	public static void main(String[] args) {
+		System.out.println(" 20240722 CiccioStart");
+		System.err.println("20240722 Ciccio err provola");
 		SpringApplication.run(SpringBootDockerApplication.class, args);
+		System.out.println("20240722 CiccioStart");
 	}
 	
     /*@GetMapping("/hello")*/
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-      return String.format("Hello %s!", name);
+		System.out.println("20240722 Ciccio out "+name);
+		System.err.println("20240722 Ciccio err "+name);
+    	return String.format("Hello %s!", name);
+     
     }
 
 }
